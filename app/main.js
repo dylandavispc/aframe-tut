14f220db97edf7606a906c8a34a18a9476e5a057
Dylan Davis <dylandavispc@gmail.com>
2020-03-08 15:27:13 -0400

    Code Commit

--- app/main.js
+++ app/main.js
@@ -12,7 +12,11 @@ const COLORS = ['#D92B6A', '#9564F2', '#FFCF59']
 class Main extends Component {
   constructor() {
     super()
-    this.state = { color: 'red' }
+    this.state = { 
+      color: 'red',
+      colorIndex: 0,
+      spherePosition: { x: 0.0, y: 4, z: -10.0 }
+    }
   }
 
   changeColor() {
@@ -22,87 +26,98 @@ class Main extends Component {
     })
   }
 
+  _handleClick() {
+    console.log('clicked')
+    this.setState({
+      colorIndex: (this.state.colorIndex + 1) % COLORS.length
+    })
+    console.log(COLORS[this.state.colorIndex])
+  }
+
   render() {
     return (
-      <Scene>
-        <a-assets>
-          <img crossOrigin id="groundTexture" src="img/floor.jpg" />
-          <img crossOrigin id="skyTexture" src="img/sky.jpg" />
-        </a-assets>
-
-        <Entity
-          primitive="a-plane"
-          src="#groundTexture"
-          rotation="-90 0 0"
-          height="100"
-          width="100"
-        />
-        <Entity primitive="a-light" type="ambient" color="#445451" />
+      <Scene 
+        effects="bloom, film, fxaa"
+        bloom="radius: 0.99"
+        film="sIntensity: 0.15; nIntensity: 0.15"
+        fxaa
+        environment={{
+          preset: 'starry',
+          seed: 2,
+          lightPosition: { x: 0.0, y: 0.03, z: -0.5 },
+          fog: 0.8,
+          ground: 'canyon',
+          groundYScale: 6.31,
+          groundTexture: 'walkernoise',
+          groundColor: '#8a7f8a',
+          grid: 'none'
+      }}>
+        {/* Lighting */}
         <Entity
           primitive="a-light"
-          type="point"
-          intensity="2"
-          position="2 4 4"
+          type="directional"
+          color="#FFF"
+          intensity={1}
+          position={{ x: 4, y: 0, z: -15 }}
         />
-        <Entity
-          primitive="a-sky"
-          height="2048"
-          radius="30"
-          src="#skyTexture"
-          theta-length="90"
-          width="2048"
-        />
-        <Entity particle-system={{ preset: 'snow', particleCount: 2000 }} />
-        <Entity
-          text={{ value: 'Hello, A-Frame Preact!', align: 'center' }}
-          position={{ x: 0, y: 2, z: -1 }}
-        />
-
-        <Entity
-          id="box"
-          geometry={{ primitive: 'box' }}
-          material={{ color: this.state.color, opacity: 0.6 }}
-          animation__rotate={{
-            property: 'rotation',
-            dur: 2000,
-            loop: true,
-            to: '360 360 360'
-          }}
-          animation__scale={{
-            property: 'scale',
-            dir: 'alternate',
-            dur: 100,
-            loop: true,
-            to: '1.1 1.1 1.1'
-          }}
-          position={{ x: 0, y: 1, z: -3 }}
-          events={{ click: this.changeColor.bind(this) }}
-        >
-          <Entity
-            animation__scale={{
-              property: 'scale',
-              dir: 'alternate',
-              dur: 100,
-              loop: true,
-              to: '2 2 2'
-            }}
-            geometry={{ primitive: 'box', depth: 0.2, height: 0.2, width: 0.2 }}
-            material={{ color: '#24CAFF' }}
-          />
-        </Entity>
-
-        <Entity primitive="a-camera">
+        {/* Camera / Cursor */}
+        <Entity primitive="a-camera" look-controls>
           <Entity
             primitive="a-cursor"
-            animation__click={{
-              property: 'scale',
-              startEvents: 'click',
-              from: '0.1 0.1 0.1',
-              to: '1 1 1',
-              dur: 150
+            cursor={{ fuse: false }}
+            material={{ color: 'white', shader: 'flat', opacity: 0.75 }}
+            geometry={{ radiusInner: 0.005, radiusOuter: 0.007 }}
+            event-set__1={{
+              _event: 'mouseenter',
+              scale: { x: 1.4, y: 1.4, z: 1.4 }
+            }}
+            event-set__2={{
+              _event: 'mouseleave',
+              scale: { x: 1, y: 1, z: 1 }
+            }}
+            raycaster={{
+              objects: '.clickable'
             }}
           />
         </Entity>
+        {/* Low-Poly Sphere */}
+        <Entity
+          class="clickable"
+          lowpoly={{
+            color: COLORS[this.state.colorIndex],
+            nodes: true,
+            opacity: 0.15,
+            wireframe: true
+          }}
+          primitive="a-octahedron"
+          detail={2}
+          events={{
+            click: this._handleClick.bind(this)
+          }}
+          radius={2}
+          position={this.state.spherePosition}
+          color="#FAFAF1"
+          animation__rotate={{
+            property: 'rotation',
+            dur: 60000,
+            easing: 'linear',
+            loop: true,
+            to: { x: 0, y: 360, z: 0 }
+          }}
+          animation__oscillate={{
+            property: 'position',
+            dur: 2000,
+            dir: 'alternate',
+            easing: 'linear',
+            loop: true,
+            from: this.state.spherePosition,
+            to: {
+              x: this.state.spherePosition.x,
+              y: this.state.spherePosition.y + 0.25,
+              z: this.state.spherePosition.z
+            }
+          }}
+        />
       </Scene>
     )
   }
